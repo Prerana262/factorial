2a817d2467bcd5571d6023e0f6ccc9c4394e6047
Prerana262 <preranamohapatra262000@gmail.com>
2023-05-07 16:18:19 +0530

    commit

---
+++ src/factorialno/FactorialNo.java
@@ -0,0 +1,15 @@
+package factorialno;
+
+public class FactorialNo {
+
+	public static void main(String[] args) {
+		int i,fact=1;
+		int no=5;
+		for(i=1;i<=no;i++) {
+			fact=fact*i;
+		}
+		System.out.println("Factorial of"+no+"is:"+fact);
+
+	}
+
+}
